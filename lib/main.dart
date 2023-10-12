44be2a0dc372aea885672eea7cba355c5b5897c4
ilobosok <121606196+ivan-lab-pvls@users.noreply.github.com>
2023-10-12 12:50:25 +0200

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'dart:async';
 import 'package:bank/app_theme.dart';
 import 'package:bank/constants.dart';
+import 'package:bank/firebase_options.dart';
+import 'package:bank/notification_service%20(1).dart';
 import 'package:bank/router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await NotificationServiceFb().activate();
   final sharedPrefs = await SharedPreferences.getInstance();
   final StreamController<bool> updateBalanceStream =
       StreamController<bool>.broadcast();
